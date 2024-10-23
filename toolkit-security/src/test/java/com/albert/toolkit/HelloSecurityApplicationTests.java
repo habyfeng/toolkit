990dcd7fd7457f4fe2316c73e251d37217d1db95
habyfeng <haby_feng@163.com>
2024-10-23 16:35:58 +0800

    新增toolkit-security模块

---
+++ toolkit-security/src/test/java/com/albert/toolkit/HelloSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albert.toolkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
